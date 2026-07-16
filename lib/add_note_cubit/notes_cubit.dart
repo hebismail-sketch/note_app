dc7507307979d774ab2d3f84ef5ecd6141293547
heba <esmailheba31@gmail.com>
2026-07-16 16:16:01 +0300

    feat:add note search functionality

--- lib/add_note_cubit/notes_cubit.dart
+++ lib/add_note_cubit/notes_cubit.dart
@@ -1,7 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
-import 'package:note_app/constants.dart';
+import 'package:note_app/core/constants.dart';
 
 import '../models/note_model.dart';
 
@@ -10,17 +10,24 @@ part 'notes_state.dart';
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
   List<NoteModel>? notes;
+  List<NoteModel>? allNotes;
 
-  fetchAllNotes(){
+  fetchAllNotes() {
+    var notesBox = Hive.box<NoteModel>(kNotesBox);
+    allNotes = notesBox.values.toList();
 
-
-    var notesBox=Hive.box<NoteModel>(kNotesBox);
-
-List<NoteModel> notes = notesBox.values.toList();
-emit(NotesSuccess());
-
-
-
-  }
+    notes = List.from(allNotes!);
+    emit(NotesSuccess());
   }
 
+  void searchNotes(String query) {
+    if (query.isEmpty) {
+      notes = List.from(allNotes!);
+    }else{notes=allNotes!.where((note){
+
+      return note.title.toLowerCase().contains(query.toLowerCase())||
+
+          note.subTitle.toLowerCase().contains(query.toLowerCase());
+    }).toList();}emit(NotesSuccess());
+  }
+}
